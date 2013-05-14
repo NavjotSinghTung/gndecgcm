0296f08c5b831f70dfe58272847c5f27c673bded
Navjot Singh <tungnavjot@gmail.com>
2013-05-14 14:54:23 +0530

    config.php

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+//Database config variables
+define("DB_HOST", "localhost");
+define("DB_USER", "");
+define("DB_PASSWORD", "");
+define("DB_DATABASE", "");
+define("GOOGLE_API_KEY", "960681139230"); //Google API Key
+?>
